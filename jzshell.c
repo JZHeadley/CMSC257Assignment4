04e825db18c3a7cacdeb2681bdf9e7ecd33231e7
jzheadley <zephyrinkg@gmail.com>
2016-11-18 13:33:58 -0500

    builtins working

--- jzshell.c
+++ jzshell.c
@@ -6,31 +6,38 @@
 
 #include "jzshell.h"
 
+
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
+
+
 void printEndStatus(int statusCode){
     if (statusCode == 0){
-        printf("The command worked with status code %i\n",statusCode);
+        fprintf(stdout,"The command worked with status code %i\n",statusCode);
     } else {
-        printf("The command failed with status code %i\n",statusCode);
+        fprintf(stderr,RED"The command failed with status code %i\n"RESET,statusCode);
     }
 }
 
 int main(int argc, char* argv[]){
     int i;
-    char *shellName="shell";
+    char *shellName="257sh";
     char command[50];
+    char cwd[100];
     int status;
     if(argc > 0){
         for(i=1;i<argc;i++){
             if (strcmp(argv[i],"-p")!=0){
-                //printf("was passed a shell name\n");
                 shellName=argv[i];
             }
         }
     }
 
     while(1) {
-        printf("%s> ",shellName);
+        getcwd(cwd,100);
+        printf("%s:%s> ",shellName,cwd);
         fgets(command,sizeof(command),stdin);
+        command[strlen(command)-1]='\0';
         status = parseInput(strlen(command),command);
         printEndStatus(status);
     }
--- jzshell_support.c
+++ jzshell_support.c
@@ -1,62 +1,80 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
+
 #include "jzshell.h"
 
 char *cmdArgs;
+char * cmdWithArgsForExec;
 char *cmd;
 const char delim[] = " ";
 const char *builtins[]={"cd","exit","pid","ppid"};
 int builtinSize=4;
+
 int parseInput(int size, char args[]) {
     int returnCode;
+    cmdWithArgsForExec = args;
     cmd = strtok(args,delim);
     cmdArgs=strtok(0,"\n");
-    //strncpy(cmdArgs,args+strlen(cmd),size-strlen(cmd));
     returnCode=handleCommands(cmd,cmdArgs,size);
+    return returnCode;
+}
 
+int isCommandBuiltin(char *cmd){
+    int i;
+    for(i=0;i<builtinSize;i++){
+        if(strcmp(cmd,builtins[i]) == 0)
+            return 1;
+    }
     return 0;
 }
 
 int handleCommands(char *cmd,char *args, int argsSize){
     char *string;
-    printf("cmd:\t%s\n",cmd);
-    printf("args:\t%s\n",args);
+    int returnCode=0;
     string = strtok(args,delim);
     while (string != NULL){
-        printf("%s\n",string);
+        //printf("%s\n",string);
         string = strtok(NULL,delim);
     }
-    if (isCommandBuiltin(cmd)!=0){// not a builtin
-        /*handleNonBuiltin();*/
-    }else{
-        handleBuiltin(cmd,args);
-        return 0;
+    if (isCommandBuiltin(cmd)==1){
+        returnCode = handleBuiltin(cmd,args);
+    } else{ // Not a builtin Command
+        returnCode = handleNonBuiltin(cmd,args);
     }
+    return returnCode;
 }
 
-
-int isCommandBuiltin(char *cmd){
-    int i;
-    for(i=0;i<builtinSize;i++){
-        if(builtins[i] == cmd);
-        return 1;
-    }
-    return 0;
-}
 int handleNonBuiltin(char *cmd, char* args){
-    if(strcmp(cmd,"cd")) {
+    int returnCode=0;
+    printf("handlingnonbuiltins\n");
 
-    } else if(strcmp(cmd,"exit")) {
-        exit(0);
-    } else if(strcmp(cmd,"pid")) {
-
-    } else if(strcmp(cmd,"ppid")) {
-
-    }
-    return 0;
+    execvp(cmd,cmdWithArgsForExec);
+    return returnCode;
 }
+
+
 int handleBuiltin(char *cmd,char* args){
-    return 0;
+    char cwd[100];
+    int returnCodeue=0;
+    printf("handlingbuiltins\n");
+    if(strcmp(cmd,"cd")==0) {
+        if(args==NULL)
+            getcwd(cwd,100);
+        else
+            returnCodeue=chdir(args);
+        printf("%s\n",cwd);
+    } else if(strcmp(cmd,"exit")==0) {
+        raise(SIGKILL);
+    } else if(strcmp(cmd,"pid")==0) {
+        printf("The pid of the shell is %i\n",getpid());
+    } else if(strcmp(cmd,"ppid")==0) {
+        printf("The pid of the parent process is %i\n",getppid());
+    }
+
+    return returnCodeue;
 }
